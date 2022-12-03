eb36ac9fea7e69dfb244dc3441497038cbd58401
Marc Kruiß <k.marc@a1.net>
2022-12-03 16:38:12 +0100

    implement static path generation

--- next-js-13-demo/app/todos/[todoId]/page.tsx
+++ next-js-13-demo/app/todos/[todoId]/page.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Todo } from "../../../typings";
+import {notFound} from 'next/navigation'
+
+export const dynamicParams = true;
 
 type PageProps = {
   params: {
@@ -9,7 +12,17 @@ type PageProps = {
 
 const fetchTodo = async (todoId: string) => {
   const res = await fetch(
-    `https://jsonplaceholder.typicode.com/todos/${todoId}`
+    `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+    {
+      // like getStaticProps (Static Site Generation [SSG])
+      //cache: "force-cache",
+
+      // like getServerSideProps (Server Side Rendering [SSR])
+      //cache:'no-store',
+
+      // like Incremental Static Generation [ISR]
+      next: { revalidate: 60 },
+    }
   );
 
   const todo: Todo = await res.json();
@@ -18,6 +31,9 @@ const fetchTodo = async (todoId: string) => {
 
 async function TodoPage({ params: { todoId } }: PageProps) {
   const todo = await fetchTodo(todoId);
+
+  if(!todo.id) return notFound();
+  
   return (
     <div className="p-10 bg-yellow-200 border-2 m-2 shadow-lg">
       <p>
@@ -33,3 +49,16 @@ async function TodoPage({ params: { todoId } }: PageProps) {
 }
 
 export default TodoPage;
+
+export async function generateStaticParams() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
+  const todos: Todo[] = await res.json();
+
+  // for this DEMO, we are only prebuilding the first 10 pages to avoid being rate limited by the demo api
+  const trimmedTodos = todos.splice(0, 10);
+
+  // [{todoId:'1'}, {todoID:'2'}, ...]
+  return trimmedTodos.map((todo) => ({
+    todoId: todo.id.toString(),
+  }));
+}
