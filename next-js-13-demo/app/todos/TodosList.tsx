18695f212cc4720758f1106eb08c9ca005b77786
Marc Kruiß <k.marc@a1.net>
2022-12-03 16:38:25 +0100

    todos stuff

--- next-js-13-demo/app/todos/TodosList.tsx
+++ next-js-13-demo/app/todos/TodosList.tsx
@@ -1,27 +1,25 @@
-import Link from 'next/link';
-import React from 'react'
-import { Todo } from '../../typings';
+import Link from "next/link";
+import React from "react";
+import { Todo } from "../../typings";
 
-const fetchTodos =async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
-    const todos:Todo[] = await res.json();
-    return todos;
-}
+const fetchTodos = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
+  const todos: Todo[] = await res.json();
+  return todos;
+};
 
 // as its an server component we can make it async
 async function TodosList() {
-    const todos = await fetchTodos()
+  const todos = await fetchTodos();
   return (
     <>
-    {
-        todos.map(todo =>(
-            <p key={todo.id}>
-                <Link href={`/todos/${todo.id}`}>Todo:{todo.id}</Link>
-            </p>
-        ))
-    }
+      {todos.map((todo) => (
+        <p key={todo.id}>
+          <Link href={`/todos/${todo.id}`}>Todo:{todo.id}</Link>
+        </p>
+      ))}
     </>
-  )
+  );
 }
 
-export default TodosList
+export default TodosList;
