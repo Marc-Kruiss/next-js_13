c64fbfcea303b8c1ed5c2ccfe92206c4e406e1cc
Marc Kruiß <k.marc@a1.net>
2022-12-04 17:50:31 +0100

    use google font

--- next-js-13-stream/pages/_app.tsx
+++ next-js-13-stream/pages/_app.tsx
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Inter } from "@next/font/google";
+import { Oswald } from "@next/font/google";
 
 // If loading a variable font, you dont need to specify the font weight
-const inter = Inter();
+const inter = Oswald();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />;
+    </main>
+  );
 }
