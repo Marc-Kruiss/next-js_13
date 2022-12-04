7d98bec4ed4c51e58a3af458175aec8861c57efb
Marc Kruiß <k.marc@a1.net>
2022-12-04 19:20:36 +0100

    add various ways for requests

---
+++ next-js-13-data-fetching/app/shared/navbar.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import styles from "../../styles/navbar.module.css";
+
+const routes = [
+  { name: "Server", path: "/server" },
+  { name: "Client", path: "/client" },
+  { name: "Staticprop", path: "/staticprops" },
+];
+
+function navbar() {
+  return (
+    <>
+      {routes.map((r) => (
+        <Link className={styles.navlinks} key={r.index} href={r.path}>
+          {r.name}
+        </Link>
+      ))}
+    </>
+  );
+}
+
+export default navbar;
