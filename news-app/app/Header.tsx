09e402369f533035ef9fb750b45451ced36af581
Marc Kruiß <k.marc@a1.net>
2022-12-14 11:59:30 +0100

    implement navlinks/searchbox

--- news-app/app/Header.tsx
+++ news-app/app/Header.tsx
@@ -1,5 +1,7 @@
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import NavLinks from "./NavLinks";
+import SearchBox from "./SearchBox";
 
 function Header() {
   return (
@@ -26,9 +28,9 @@ function Header() {
         </div>
       </div>
 
-      {/*NavLinks */}
+      <NavLinks />
 
-      {/*SearchBar */}
+      <SearchBox />
     </header>
   );
 }
