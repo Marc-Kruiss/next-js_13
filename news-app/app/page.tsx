d4eb237406dc551dd900ce6ca4cd8b08c7c31565
Marc Kruiß <k.marc@a1.net>
2022-12-14 14:58:27 +0100

    execute graphql query

--- news-app/app/page.tsx
+++ news-app/app/page.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { categories } from "../constants";
+import fetchNews from "../lib/fetchNews";
+import NewsList from "./NewsList";
+import response from "../response.json";
 
-function Homepage() {
-  return <div>Homepage</div>;
+async function Homepage() {
+  // fetch the news data
+  const news: NewsResponse =
+    response || (await fetchNews(categories.join(",")));
+
+  console.log(news);
+
+  return (
+    <div>
+      <NewsList news={news} />
+    </div>
+  );
 }
 
 export default Homepage;
