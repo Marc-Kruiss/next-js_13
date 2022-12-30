aed73b3e93a3ccd335c4e0b7174e06970ac44b67
Marc Kruiß <k.marc@a1.net>
2022-12-30 18:53:27 +0100

    add begin/exit preveiw api calls

--- next-js-13-sanity-blog/app/page.tsx
+++ next-js-13-sanity-blog/app/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { previewData } from "next/headers";
+import PreviewSuspense from "../components/sanity-studio/PreviewSuspense";
 
 function Homepage() {
+  if (previewData()) {
+    return (
+      <PreviewSuspense
+        fallback={
+          <div className="w-full flex items-center justify-center">
+            Loading preview...
+          </div>
+        }
+      >
+        <div>Hallo</div>
+      </PreviewSuspense>
+    );
+  }
+
   return (
-    <div>Homepage</div>
-  )
+    <div>
+      <h1 className="text-5xl">Hallo</h1>
+    </div>
+  );
 }
 
-export default Homepage
+export default Homepage;
