b67c6103d936b9aaf890f7185a51a4e514be3907
Marc Kruiß <k.marc@a1.net>
2022-12-30 18:31:51 +0100

    include own sanity desk design

--- next-js-13-sanity-blog/sanity.config.ts
+++ next-js-13-sanity-blog/sanity.config.ts
@@ -2,6 +2,9 @@ import { defineConfig, WorkspaceOptions } from "sanity";
 import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
+import { getDefaultDocumentNode } from "./structure";
+import Logo from "./components/sanity-studio/Logo";
+import StudioNavbar from "./components/sanity-studio/StudioNavbar";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
@@ -13,9 +16,20 @@ export default defineConfig<WorkspaceOptions>({
   title: "Blog Studio",
   projectId,
   dataset,
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool({
+      defaultDocumentNode: getDefaultDocumentNode,
+    }),
+    visionTool(),
+  ],
 
   schema: {
     types: schemaTypes,
   },
+  studio:{
+    components:{
+      logo:Logo,
+      navbar:StudioNavbar,
+    }
+  }
 });
